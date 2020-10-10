644564ce7c68e6ab9fc4933e0b15ef4accee8d80
liuxiangdong3@xdf.cn <liuxiangdong3@xdf.cn>
2020-10-10 18:55:37 +0800

    feat: commit

--- app/controller/user.js
+++ app/controller/user.js
@@ -4,65 +4,13 @@ const Controller = require('egg').Controller;
 const info = require('../utils/info');
 
 class UserController extends Controller {
+
   async index () {
     const { ctx, app } = this;
     ctx.body = 'user index';
   }
 
-  async lists() {
-    const { ctx } = this;
-    // const res = await ctx.service.user.lists();
-
-    const res = await ctx.model.User.findAll();
-    ctx.body=res;
-  }
-
-  async find() {
-    const { ctx } = this;
-    // const res = await ctx.service.user.find(1);
-    const res = await ctx.model.User.findByPk(ctx.query.id); //查询指定数据
-    ctx.body = res;
-  }
-
   
-  async insert() {
-    const { ctx } = this;
-    const user = {
-      id: 5,
-      name: 'user2',
-      pwd: '123'
-    }
-    // const res = await ctx.service.user.insert(user);
-    const res = await ctx.model.User.create(user);
-    ctx.body = res;
-  }
-
-  async update() {
-    const { ctx } = this;
-    const params = {
-      id: 7,
-      name: "user2222",
-      pwd: "222"
-    }
-
-    // const res = await ctx.service.user.update(user);
-
-    const user = await ctx.model.User.findByPk(JSON.stringify(params.id));
-    if (!user) {
-      ctx.status = 404;
-      return;
-    }
-    const res = await user.update(params);
-
-    ctx.body = res;
-  }
-
-  async delete() {
-    const { ctx } = this;
-    const user ={ name: 'user2' };
-    const res = await ctx.service.user.delete(user);
-    ctx.body = res;
-  }
 
 }
 
--- app/router.js
+++ app/router.js
@@ -6,14 +6,13 @@
 module.exports = app => {
   const { router, controller, jsonp } = app;
   router.get('/', controller.home.index);
-  router.get('/user', controller.user.index);
-  router.get('/user/lists',controller.user.lists);
-  router.get('/user/find', controller.user.find);
-  router.get('/user/insert', controller.user.insert);
-  router.get('/user/update', controller.user.update);
-  router.get('/user/delete', controller.user.delete);
+  // router.get('/user', controller.user.index);
+  // router.get('/user/lists',controller.user.lists);
+  // router.get('/user/find', controller.user.find);
+  // router.get('/user/insert', controller.user.insert);
+  // router.get('/user/update', controller.user.update);
+  // router.get('/user/delete', controller.user.delete);
   router.get('/demo', controller.home.demo);
-
   router.get('/gannt/lists', controller.gannt.lists);
   router.post('/gannt/created', controller.gannt.created);
   router.post('/gannt/update', controller.gannt.update);
