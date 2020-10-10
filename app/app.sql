37f66293e3d31a5f58522aa8b55a037a3bb9b7d2
liuxiangdong3@xdf.cn <liuxiangdong3@xdf.cn>
2020-10-10 17:33:33 +0800

    edit: 修改user表

--- app/app.sql
+++ app/app.sql
@@ -2,15 +2,14 @@ create database egg_house;
 
 use egg_house;
 
--- 用户表
 create table `user`(
   `id` int not null auto_increment,
   `username` varchar(20) default null comment '用户名',
   `password` varchar(64) default null comment '密码',
-  `avatar` text comment '头像',
-  `e-mail` varchar(20) detault null comment '电话',
+  `phone` varchar(20) default null comment '电话',
   `sign` varchar(300) default null comment '用户签名',
-  `createTime` timestamp default null comment '创新时间',
-  `updateTime` timestamp default null comment '更新时间',
-  primary key(`id`) 
-)engine=InnoDB auto_increment=1 default charset=utf-8 comment='用户表'
+  `createTime` timestamp comment '创新时间',
+  `updateTime` timestamp default '1970-01-01 10:00:00' comment '更新时间',
+  `avatar` text comment '头像',
+  primary key(`id`)
+)engine=InnoDB auto_increment=1 default charset=utf8 comment='用户表';
--- app/model/user.js
+++ app/model/user.js
@@ -1,17 +1,15 @@
 module.exports = app => {
-  const { STRING, INTEGER } = app.Sequelize;
+  const { STRING, INTEGER, TEXT, DATE } = app.Sequelize;
 
   const User = app.model.define('user', {
     id: { type: INTEGER, primaryKey: true, autoIncrement: true }, //  设置主键  自增长 
-    label: STRING(20),
-    user: STRING(20),
-    start: INTEGER(20),
-    endDate: STRING(20),
-    duration: INTEGER(50),
-    percent: INTEGER(10),
-    type: STRING(20),
-    proType: STRING(20),
-    risk: STRING(10),
+    username: STRING(20),
+    password: STRING(64),
+    avatar: TEXT('long'),
+    phone: STRING(20),
+    sign: STRING(300),
+    createTime: DATE,
+    updateTime: DATE
   })
 
   return User;
